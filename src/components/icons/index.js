9d09f36203da200f46e0414c1847f9f15b48d3e3
piheta <m@picheta.dev>
2026-06-17 16:36:31 +0200

    v3

--- src/components/icons/index.js
+++
@@ -1,4 +0,0 @@
-// Icon components barrel exports
-export { default as FileIcon } from './FileIcon'
-export { default as FolderIcon } from './FolderIcon'
-export { default as LinkIcon } from './LinkIcon'
